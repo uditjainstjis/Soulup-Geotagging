6c5a7f9385a87613bd09e6e697034183e71a097c
Udit Jain <uditjainstjis@gmail.com>
2025-03-28 18:29:07 +0530

    details page loading before auth fix

--- src/app/components/MapComp.jsx
+++ src/app/components/MapComp.jsx
@@ -1,57 +1,36 @@
 "use client";
 
 import { useEffect, useState, useContext } from 'react';
-import React,{useCallback} from 'react';
+import React from 'react';
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
-import { useRouter } from 'next/navigation'
-//custom PoiMarkers Imported
-import PoiMarkers from './PoiMarkers'
-import {MainLocations, ZoomLocations} from './contexts'
-import { useUserLocation } from './useLocation'
+import { useRouter } from 'next/navigation';
+// Custom PoiMarkers Imported
+import PoiMarkers from './PoiMarkers';
+import { MainLocations, ZoomLocations } from './contexts';
+import { useUserLocation } from './useLocation';
 import UserDetailsForm from '../components/UserDetailsForm';
 
-
 const MapComp = () => {
-  var {Locs, setLocs} = useContext(MainLocations)
-  var {ZoomLocs, setZoomLocs} = useContext(ZoomLocations)
-  var {Zoom, setZoom} = useContext(ZoomLocations)
-
+  var { Locs, setLocs } = useContext(MainLocations);
+  var { ZoomLocs, setZoomLocs } = useContext(ZoomLocations);
+  var { Zoom, setZoom } = useContext(ZoomLocations);
 
   const [error, setError] = useState(null);
-  const router = useRouter()
-
-  useEffect(() => {
-    async function checkdetails(){
-    const check = await fetch('/api/userdetails')
-    const userDetails = await check.json();
-    if(!userDetails.gender){
-      
-      router.push('details')
-    }
-  }
-  checkdetails()
-  }, [])
-
+  const router = useRouter();
 
   useEffect(() => {
     async function getLocations() {
       try {
         const res = await fetch("/api/Loc"); // Calls Next.js API route
-
         const data = await res.json();
         if (res.ok) {
-          setLocs(data)
-          // console.log("data from server to plot locations", data
-
-          // console.log(Locs)
-          console.log("data from server to plot locations", data)
-
+          setLocs(data);
+          console.log("data from server to plot locations", data);
         } else {
-          if(res.status==401){
-
-            router.push('/signin')
-          }else{
-          throw new Error("Failed to fetch location ;(",res);
+          if (res.status == 401) {
+            router.push('/signin');
+          } else {
+            throw new Error("Failed to fetch location", res);
           }
         }
       } catch (error) {
@@ -59,19 +38,25 @@ const MapComp = () => {
         setError("Failed to fetch location");
       }
     }
-    getLocations();
-  },[])
-
+    getLocations().then(() => {
+      async function checkDetails() {
+        const check = await fetch('/api/userdetails');
+        const userDetails = await check.json();
+        if (!userDetails.gender) {
+          router.push('details');
+        }
+      }
+      checkDetails();
+    });
+  }, []);
 
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} >
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
       <Map
-        // style={{ width: '60vw', height: '60vh'}}
         defaultCenter={ZoomLocs}
         defaultZoom={Zoom}
         gestureHandling={'greedy'}
         mapId={'6d4a78bd07042b18'}
-
       >
         <PoiMarkers pois={Locs} />
       </Map>
