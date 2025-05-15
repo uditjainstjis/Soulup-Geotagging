641bb8779e495bd44e941956e17672820aa6f6ea
Udit Jain <uditjainstjis@gmail.com>
2025-05-15 17:11:15 +0530

    fullycomplete

--- src/app/components/buttons.jsx
+++ src/app/components/buttons.jsx
@@ -4,7 +4,7 @@ const Buttons = ({ showButton, tellButton, handleFirstButton, handleTellPeople }
     return (
         <>
             {showButton && (
-                <div className="flex flex-col  justify-end">
+                <div className="flex flex-col md:mt-0 mt-[70vh] justify-end">
                     <button
                         className="bg-yellow md:mt-3  rounded-full px-4 text-bold h-[3.25rem] w-full text-white self-end border-1 transition-transform duration-200 active:scale-95" // Added transition and active scale
                         onClick={handleFirstButton}
@@ -15,7 +15,7 @@ const Buttons = ({ showButton, tellButton, handleFirstButton, handleTellPeople }
             )}
 
             {(!showButton && tellButton )&& (
-                <div className="flex flex-col justify-end mt-[-5px]">
+                <div className="flex flex-col  mt-[35vh] justify-end md:mt-[-5px]">
                     <button
                         className="animate-fade-in bg-yellow border-2 border-orange-200 mt-3 rounded-full px-4 text-bold h-[3.25rem] w-full text-white self-end border-1 transition-all duration-300 active:scale-105 active:bg-yellow-600 active:border-yellow-600" // Added more pronounced active effect
                         onClick={handleTellPeople}
